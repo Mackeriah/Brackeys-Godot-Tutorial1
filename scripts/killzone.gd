c66062d7b825ceea4d9db4540c48bc129a75bee1
Mackeriah <fitzgerald.owen@gmail.com>
2024-05-04 07:21:42 +1000

    various
    - controls setup
    - map updates
    - enemies
    - player death improved

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,9 +4,12 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(_body):
-	print("player died MOFO!")
+	print("player died!")
+	Engine.time_scale = 0.5
+	_body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout():
 	# when timer ends reload scene
 	get_tree().reload_current_scene()
+	Engine.time_scale = 1.0
