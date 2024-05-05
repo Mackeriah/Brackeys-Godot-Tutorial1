c8d7e1f1e772cda1880f6b18bae07767e7252820
Mackeriah <fitzgerald.owen@gmail.com>
2024-05-05 17:06:56 +1000

    WIP

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,9 +3,13 @@ extends Area2D
 @onready var timer = $Timer
 @onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
+@onready var player = $"../Player"
 
 func _on_body_entered(_body):		
-	animation_player.play("PlayerDeathSound")
+	
+	# if player hasn't turnred off sounds
+	if game_manager.quietPlease == false:
+		animation_player.play("PlayerDeathSound")
 	Engine.time_scale = 0.5	
 	_body.get_node("CollisionShape2D").queue_free()	
 	timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,12 +5,13 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var animation_player = $AnimationPlayer
+@onready var game_manager = %GameManager
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+	print(game_manager.quietPlease)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -19,11 +20,12 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-		# randomly play a jumping sound
-		if (randi() % 10) >= 6:		
-			animation_player.play("playerJump")
-		else:
-			pass
+		# randomly play a jumping sound IF player hasn't turned off sounds
+		if game_manager.quietPlease == false:
+			if (randi() % 10) >= 6:	
+				animation_player.play("playerJump")
+			else:
+				pass
 		
 	# Get the input direction 0, 1, -1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -42,7 +44,7 @@ func _physics_process(delta):
 		else:
 			animated_sprite.play("run")
 	else:
-		animated_sprite.play("jumping")			
+		animated_sprite.play("jumping")
 	
 	if direction:
 		velocity.x = direction * SPEED
@@ -50,3 +52,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	# allow player to turn off character talking
+	if Input.is_action_just_pressed("no talking") and game_manager.quietPlease == false:
+		game_manager.quietPlease = true		
+	elif Input.is_action_just_pressed("no talking") and game_manager.quietPlease == true:
+		game_manager.quietPlease = false		
