50b1166243bae281341348d3edc389d725cc1482
Mackeriah <fitzgerald.owen@gmail.com>
2024-05-05 21:29:19 +1000

    various
    Essentially was trying to fix a bug whereby on collision with enemy the game crashes. I suspect it's to do with ordering but I don't know how that works in Godot yet as the tutorial didn't cover it.
    
    Not really crucial to fix as I was trying to introduce a feature where you can press X to disable sounds, which does work for jumping sound but crashes on enemy related death. It's to do with how the killzone was created for 100% sure but again, as it was a tutorial and details were not covered, it's not something I can figure out yet, nor is it worth any further effort.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,14 +1,21 @@
 extends Area2D
 
 @onready var timer = $Timer
-@onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
 @onready var player = $"../Player"
+@onready var game_manager = %GameManager
 
-func _on_body_entered(_body):		
-	
-	# if player hasn't turnred off sounds
-	# if game_manager.quietPlease == false:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	check_game_manager()
+
+ # if player hasn't turnred off sounds
+func check_game_manager():
+	pass
+	#if game_manager.score == 0:
+		#print("hello world")
+
+func _on_body_entered(_body):			
 	animation_player.play("PlayerDeathSound")
 	Engine.time_scale = 0.5	
 	_body.get_node("CollisionShape2D").queue_free()	
