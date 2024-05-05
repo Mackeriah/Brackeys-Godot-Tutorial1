627804f957d9de558210fad864c35b7b272d62b4
Mackeriah <fitzgerald.owen@gmail.com>
2024-05-05 10:50:27 +1000

    various
    - improved player sound effects
    - exported

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,12 @@ extends Area2D
 # create a variable and reference it to the timer node ($ is the path)
 @onready var timer = $Timer
 @onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(_body):		
 	Engine.time_scale = 0.5	
 	_body.get_node("CollisionShape2D").queue_free()
-	
+	animation_player.play("PlayerDeathSound")
 	timer.start()
 
 func _on_timer_timeout():
