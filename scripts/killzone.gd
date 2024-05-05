92344e245b17787b6b3d4fa9c59e22dbf2084a03
Mackeriah <fitzgerald.owen@gmail.com>
2024-05-05 13:05:55 +1000

    bug fixes
    - music stopped when outside of range
    - no player death sound later in level

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
-# create a variable and reference it to the timer node ($ is the path)
 @onready var timer = $Timer
 @onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(_body):		
-	Engine.time_scale = 0.5	
-	_body.get_node("CollisionShape2D").queue_free()
 	animation_player.play("PlayerDeathSound")
+	Engine.time_scale = 0.5	
+	_body.get_node("CollisionShape2D").queue_free()	
 	timer.start()
 
 func _on_timer_timeout():
